c4a303b6a057f853251889687259c59d06c23458
NadirDAM <nadir.elhaidouri@institutvidreres.cat>
2024-11-20 00:11:37 +0100

    Add Toast On Click

--- app/src/main/java/com/example/dam2_projectealumnesprova/LlistatAlumnes.kt
+++ app/src/main/java/com/example/dam2_projectealumnesprova/LlistatAlumnes.kt
@@ -19,39 +19,33 @@ class LlistatAlumnes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // setContentView(R.layout.activity_llistat_alumnes)
-        val binding : ActivityLlistatAlumnesBinding = DataBindingUtil.setContentView(this, R.layout.activity_llistat_alumnes)
+        val binding: ActivityLlistatAlumnesBinding = DataBindingUtil.setContentView(this, R.layout.activity_llistat_alumnes)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.spinnerCurs.setAdapter(ArrayAdapter<CursEnum>(this, android.R.layout.simple_spinner_item, CursEnum.values()));
-        // getting the recyclerview by its id
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
-        // this creates a vertical layout Manager
+        binding.spinnerCurs.setAdapter(ArrayAdapter<CursEnum>(this, android.R.layout.simple_spinner_item, CursEnum.values()))
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
         recyclerview.layoutManager = LinearLayoutManager(this)
 
-        // ArrayList of class ItemsViewModel
+        val listItems = DataSource.listItemsViewModel
 
-        // This will pass the ArrayList to our Adapter
-        var adapter = CustomAdapter(DataSource.listItemsViewModel)
+        val adapter = CustomAdapter(listItems) { item ->
+            Toast.makeText(this, "Nom: ${item.nom}, Edat: ${item.edat}, Curs: ${item.curs}", Toast.LENGTH_SHORT).show()
+        }
 
-        // Setting the Adapter with the recyclerview
         recyclerview.adapter = adapter
 
-        binding.button.setOnClickListener{
-            val newData = DataSource.listItemsViewModel.filter { it.curs.toString()  == binding.spinnerCurs.selectedItem.toString() }
-            adapter = CustomAdapter(newData);
-            recyclerview.adapter = adapter
+        binding.button.setOnClickListener {
+            val newData = listItems.filter { it.curs.toString() == binding.spinnerCurs.selectedItem.toString() }
+            val newAdapter = CustomAdapter(newData) { item ->
+                Toast.makeText(this, "Nom: ${item.nom}, Edat: ${item.edat}, Curs: ${item.curs}", Toast.LENGTH_SHORT).show()
+            }
+            recyclerview.adapter = newAdapter
         }
-
-        binding.recyclerview.setOnClickListener{ item: View? ->
-            System.out.println(item)
-            Toast.makeText(this, "Clic en: ${item}", Toast.LENGTH_SHORT).show()
-        }
-
     }
 }
